3f493459beaf2b956f9ceb70423c0c6f91dc2d89
Александр Свешников <a.sveshnikov@astrotech-co.ru>
2016-05-21 19:19:34 +0300

    [+] select2 & icheck scripts added

--- resources/views/admin/layouts/partials/scripts.blade.php
+++ resources/views/admin/layouts/partials/scripts.blade.php
@@ -1,5 +1,7 @@
 <script src="{{ asset('/adm/plugins/jQuery/jQuery-2.1.4.min.js') }}" type="text/javascript"></script>
 <script src="{{ asset('/adm/js/bootstrap.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('/adm/plugins/select2/select2.min.js') }}"></script>
+<script src="{{ asset('/adm/plugins/iCheck/icheck.min.js') }}"></script>
 <script src="{{ asset('/adm/js/app.min.js') }}" type="text/javascript"></script>
 <script src="{{ asset('/adm/plugins/slimScroll/jquery.slimscroll.min.js') }}" type="text/javascript"></script>
 <script src="{{ asset('/adm/plugins/fastclick/fastclick.min.js') }}" type="text/javascript"></script>
