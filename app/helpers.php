cb8fb428ea2c5e9addfbef60e0e49fdcd2f6815a
Александр Свешников <a.sveshnikov@astrotech-co.ru>
2016-06-28 00:25:34 +0300

    [-] admin function removed

--- app/helpers.php
+++ app/helpers.php
@@ -1,15 +1,23 @@
 <?php
 
+    use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
     if ( !function_exists( 'user' ) )
     {
         /**
          * @param $field
          *
-         * @return App\Models\User|string
+         * @return \App\Models\User|string
+         * @throws AccessDeniedHttpException
          */
         function user( $field = null )
         {
-            $user = Auth::guard( 'user' )->user();
+            if( Auth::guest() )
+            {
+                throw new AccessDeniedHttpException('Unauthorized user');
+            }
+
+            $user = Auth::user();
 
             if(!$field)
             {
@@ -17,25 +25,5 @@
             }
 
             return property_exists( $user, $field ) ? $user->$field : '';
-        }
-    }
-
-    if ( !function_exists( 'admin' ) )
-    {
-        /**
-         * @param $field
-         *
-         * @return App\Models\Admin\User|string
-         */
-        function admin( $field = null )
-        {
-            $admin = Auth::guard( 'admin' )->user();
-
-            if(!$field)
-            {
-                return $admin;
-            }
-
-            return property_exists( $admin, $field ) ? $admin->$field : '';
         }
     }
